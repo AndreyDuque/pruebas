80942bf20ec7001dc9b767e0145e422faa881284
Luillin Escobar <luillin.escobar@hotmail.com>
2022-04-22 20:52:47 -0500

    se agrega multiplicar y dividir

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ class Calculadora:
 
     def restar(self):
         print(f'{self.numero1 - self.numero2}')
+    
+    def multiplicar(self):
+        print(f'{self.numero1 * self.numero2}')
+    
+    def dividir(self):
+        print(f'{self.numero1 / self.numero2}')
 
 
 operaciones = Calculadora(20, 5)
