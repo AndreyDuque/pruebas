5bd7e6913a356ad5980010dae8f85101c1f31b45
asantanadurango <asantanadurango@gmail.com>
2022-04-22 20:38:07 -0500

    se crea metodo sumar y restar en clase Calculadora

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ class Calculadora:
     def __init__(self, number1, number2):
         self.numero1 = number1
         self.numero2 = number2
+        self.add()
+        self.subtract()
+
+    def sumar(self):
+        print(f'{self.numero1 + self.numero2}')
+
+    def restar(self):
+        print(f'{self.numero1 - self.numero2}')
+
+
+operaciones = Calculadora(20, 5)
